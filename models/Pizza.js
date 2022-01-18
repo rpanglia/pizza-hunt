e9e84346cdf5b9bd68661e78b34e6512a003442e
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-18 16:37:41 -0500

    spelling

--- models/Pizza.js
+++ models/Pizza.js
@@ -37,40 +37,40 @@ const PizzaSchema = new Schema(
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.reduce((total, commment) => total + commentCount.replies.length + 1, 0);
+  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
-// const commentCount = new Schema (
-//   {
-//     commentName: {
-//       type: String
-//     },
-//     createdBy: {
-//       type: String
-//     },
-//     createdAt: {
-//       type: Date,
-//       default: Date.now,
-//       get: (createdAtVal) => dateFormat(createdAtVal)
-//     },
-//     comments: [
-//       {
-//         type: Schema.Types.ObjectId,
-//         ref: 'Comment'
-//       }
-//     ]
-//   },
-//   {
-//     toJSON: {
-//       virtuals: true,
-//       getters: true
-//     },
-//     id: false
-//   }
-// );
+const commentCount = new Schema (
+  {
+    commentName: {
+      type: String
+    },
+    createdBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
 
 
 
